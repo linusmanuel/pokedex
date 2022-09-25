293d993a4d55eeb69c6429a4b849f0a6b7cd203f
Joao Lino <linusmannuel@gmail.com>
2022-09-25 13:18:14 +0100

    feat(pokedex): Getting the pokemon from pokeapi using axios

--- src/pokedex/pokedex.tsx
+++ src/pokedex/pokedex.tsx
@@ -1,26 +1,46 @@
-import React, {useState} from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+
 
 interface PokedexProps {
 
 }
+interface PokemonListInterface {
+  name: string;
+  url: string;
+}
 
-const pokemonsArray: string[] = ['Pikachu', 'Ditto', 'Metapod', 'Magikarp']
 
 export const Pokedex: React.FC<PokedexProps> = () =>{
-  const [pokemons, setPokemons] = useState<string[]>(pokemonsArray);
-  const [selectedPokemon, setSelectedPokemon] = useState<string | undefined>(undefined);
+  const [pokemons, setPokemons] = useState<PokemonListInterface[]>([]);
+  const [selectedPokemon, setSelectedPokemon] = useState<PokemonListInterface | undefined>(undefined);
+  const [selectedPokemonDatails, setSelectedPokemonDatails] = useState<any | undefined>(undefined);
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon').then(( response ) => setPokemons(response.data.results));
+  }, []);
+  
+  useEffect(() => {
+    if (!selectedPokemon) return;
+
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon.name}`).then((response) => setSelectedPokemonDatails(response.data));
+
+  }, [selectedPokemon]);
 
   return (
 
     <div className="bloco">
+
       <h1 className="c-title c-title--1">Pokedex</h1>
 
       Pokemons: 
-      {pokemons.map((pokemon) => <button onClick={() => setSelectedPokemon(pokemon)}>{ pokemon }</button>)}
+      {pokemons.map((pokemon) => <button onClick={() => setSelectedPokemon(pokemon)}>{pokemon.name}</button>)}
 
-      <h2 className="c-title c-title--2">Pokemon selecionado: { selectedPokemon || 'Nenhum Pokemon foi encontrado...'}</h2>
+      <h2>Pokemon Seleionado {selectedPokemon?.name || "Nenhum pokemon selecionado"}</h2>
+
+      {JSON.stringify(selectedPokemonDatails, undefined, 2)}
     </div>
-  )
-}
+  );
+};
 
 export default Pokedex;
